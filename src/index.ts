d49e76452986d58c050c23febed5bc78b76983a3
Vlady <contact.johngoldco@gmail.com>
2022-10-10 18:21:38 +0300

    Add uuid

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
+import nanoid
+
 const list = document.querySelector<HTMLUListElement>("#list");
 const form = document.querySelector<HTMLFormElement>("#new-task-form");
 const input = document.querySelector<HTMLInputElement>("#new-task-title");
+
+form?.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  if (input?.value === "" || input?.value === null) return;
+
+  const task = {
+    id: nanoid()
+    title: input.value,
+    completed: false,
+    createdAt: new Date()
+  }
+});
